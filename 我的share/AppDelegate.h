4ac00dc1e757c8e6bb7910c2d7d49b1f929158d3
王恬 <wangtian@wangtiandeMacBook-Air.local>
2017-07-26 14:19:28 +0800

    Initial Commit

---
+++ 我的share/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  我的share
+//
+//  Created by 王恬 on 2017/7/26.
+//  Copyright © 2017年 王恬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 我的shareTests/__shareTests.m
@@ -0,0 +1,39 @@
+//
+//  __shareTests.m
+//  我的shareTests
+//
+//  Created by 王恬 on 2017/7/26.
+//  Copyright © 2017年 王恬. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface __shareTests : XCTestCase
+
+@end
+
+@implementation __shareTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
